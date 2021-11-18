b9acac80ed8d83a411ac33fea58dd68291be7143
GeraldineBB <geraldine-bornet@hotmail.fr>
2021-11-18 19:44:59 +0100

    Use dispatch dans le composant appHeader pour récupérer les références des actions du reducer .. mais ils devraient etre dans le middleware apparemment

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -6,17 +6,31 @@ import logo from 'src/assets/logo.png';
 
 import LoginForm from 'src/components/LoginForm';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 const AppHeader = () => {
   const email = useSelector((state) => state.user.emailValue);
   const password = useSelector((state) => state.user.passwordValue);
   const isLogged = useSelector((state) => state.user.isLogged);
 
+  const dispatch = useDispatch();
+
+  const changeField = () => {
+    dispatch(changeField);
+  };
+
+  const handleLogin = () => {
+    dispatch(handleLogin);
+  };
+
+  const handleLogout = () => {
+    dispatch(handleLogout);
+  };
+
   return (
     <header className="header">
       <img src={logo} className="header-logo" alt="Logo oRecipes" />
-      <LoginForm email={email} password={password} isLogged={isLogged} />
+      <LoginForm email={email} password={password} isLogged={isLogged} changeField={changeField} handleLogin={handleLogin} handleLogout={handleLogout} />
     </header>
   );
 };
